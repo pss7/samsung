1f9bc1aea6fb15463041fa75085d2b7171c28f0c
parksangseok <>
2024-11-25 15:51:46 +0900

    commit

--- js/sub.js
+++ js/sub.js
@@ -15,21 +15,33 @@ $(function () {
     loop: false,
     slidesPerView: 'auto',
     navigation: {
-      nextEl: '.productWrap .nextBtn', 
-      prevEl: '.productWrap .prevBtn', 
+      nextEl: '.productWrap .nextBtn',
+      prevEl: '.productWrap .prevBtn',
     },
     on: {
       init: function () {
         const totalSlides = this.slides.length;
-        const currentSlide = this.activeIndex + 1; 
+        const currentSlide = this.activeIndex + 1;
         document.querySelector('.totalSlides').textContent = totalSlides;
         document.querySelector('.currentSlide').textContent = currentSlide;
       },
       slideChange: function () {
-        const currentSlide = this.activeIndex + 1; 
+        const currentSlide = this.activeIndex + 1;
         document.querySelector('.currentSlide').textContent = currentSlide;
       },
     }
+
+  });
+
+  $('.productWrap .productList01 li h3 a').click(function () {
+
+    if ($(this).parent().hasClass('active')) {
+      $(this).parent().removeClass('active');
+    } else {
+      $(this).parent().addClass('active');
+    }
+
+    return false;
 
   });
 
