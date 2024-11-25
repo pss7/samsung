77897b6309265a816b476d111d640b301ec0f576
parksangseok <>
2024-11-25 17:07:53 +0900

    commit

--- js/sub.js
+++ js/sub.js
@@ -11,73 +11,273 @@ $(function () {
     }
   });
 
-  const swiper02 = new Swiper('.tabContentSlide01 .swiper-container', {
-    loop: false,
-    slidesPerView: 'auto',
-    navigation: {
-      nextEl: '.tabContentSlide01 .nextBtn',
-      prevEl: '.tabContentSlide01 .prevBtn',
-    },
-    on: {
-      init: function () {
-        const totalSlides = this.slides.length;
-        const currentSlide = this.activeIndex + 1;
-        document.querySelector('.tabContentSlide01 .totalSlides').textContent = totalSlides;
-        document.querySelector('.tabContentSlide01 .currentSlide').textContent = currentSlide;
-      },
-      slideChange: function () {
-        const currentSlide = this.activeIndex + 1;
-        document.querySelector('.tabContentSlide01 .currentSlide').textContent = currentSlide;
-      },
-    }
-
-  });
-
-  const swiper03 = new Swiper('.tabContentSlide02 .swiper-container', {
-    loop: false,
-    slidesPerView: 'auto',
-    navigation: {
-      nextEl: '.tabContentSlide02 .nextBtn',
-      prevEl: '.tabContentSlide02 .prevBtn',
-    },
-    on: {
-      init: function () {
-        const totalSlides = this.slides.length;
-        const currentSlide = this.activeIndex + 1;
-        document.querySelector('.tabContentSlide02 .totalSlides').textContent = totalSlides;
-        document.querySelector('.tabContentSlide02 .currentSlide').textContent = currentSlide;
-      },
-      slideChange: function () {
-        const currentSlide = this.activeIndex + 1;
-        document.querySelector('.tabContentSlide02 .currentSlide').textContent = currentSlide;
-      },
-    }
-
-  });
-
-  const swiper04 = new Swiper('.tabContentSlide03 .swiper-container', {
-    loop: false,
-    slidesPerView: 'auto',
-    navigation: {
-      nextEl: '.tabContentSlide03 .nextBtn',
-      prevEl: '.tabContentSlide03 .prevBtn',
-    },
-    on: {
-      init: function () {
-        const totalSlides = this.slides.length;
-        const currentSlide = this.activeIndex + 1;
-        document.querySelector('.tabContentSlide03 .totalSlides').textContent = totalSlides;
-        document.querySelector('.tabContentSlide03 .currentSlide').textContent = currentSlide;
-      },
-      slideChange: function () {
-        const currentSlide = this.activeIndex + 1;
-        document.querySelector('.tabContentSlide03 .currentSlide').textContent = currentSlide;
-      },
-    }
-
-  });
-
   /* 제품 */
+  const swiper02 = new Swiper('.productSlide01 .swiper-container', {
+    loop: false,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.productSlide01 .nextBtn',
+      prevEl: '.productSlide01 .prevBtn',
+    },
+    on: {
+      init: function () {
+        const totalSlides = this.slides.length;
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide01 .totalSlides').textContent = totalSlides;
+        document.querySelector('.productSlide01 .currentSlide').textContent = currentSlide;
+      },
+      slideChange: function () {
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide01 .currentSlide').textContent = currentSlide;
+      },
+    }
+
+  });
+
+  const swiper03 = new Swiper('.productSlide02 .swiper-container', {
+    loop: false,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.productSlide02 .nextBtn',
+      prevEl: '.productSlide02 .prevBtn',
+    },
+    on: {
+      init: function () {
+        const totalSlides = this.slides.length;
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide02 .totalSlides').textContent = totalSlides;
+        document.querySelector('.productSlide02 .currentSlide').textContent = currentSlide;
+      },
+      slideChange: function () {
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide02 .currentSlide').textContent = currentSlide;
+      },
+    }
+
+  });
+
+  const swiper04 = new Swiper('.productSlide03 .swiper-container', {
+    loop: false,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.productSlide03 .nextBtn',
+      prevEl: '.productSlide03 .prevBtn',
+    },
+    on: {
+      init: function () {
+        const totalSlides = this.slides.length;
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide03 .totalSlides').textContent = totalSlides;
+        document.querySelector('.productSlide03 .currentSlide').textContent = currentSlide;
+      },
+      slideChange: function () {
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide03 .currentSlide').textContent = currentSlide;
+      },
+    }
+
+  });
+
+  const swiper05 = new Swiper('.productSlide04 .swiper-container', {
+    loop: false,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.productSlide04 .nextBtn',
+      prevEl: '.productSlide04 .prevBtn',
+    },
+    on: {
+      init: function () {
+        const totalSlides = this.slides.length;
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide04 .totalSlides').textContent = totalSlides;
+        document.querySelector('.productSlide04 .currentSlide').textContent = currentSlide;
+      },
+      slideChange: function () {
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide04 .currentSlide').textContent = currentSlide;
+      },
+    }
+
+  });
+
+  const swiper06 = new Swiper('.productSlide05 .swiper-container', {
+    loop: false,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.productSlide05 .nextBtn',
+      prevEl: '.productSlide05 .prevBtn',
+    },
+    on: {
+      init: function () {
+        const totalSlides = this.slides.length;
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide05 .totalSlides').textContent = totalSlides;
+        document.querySelector('.productSlide05 .currentSlide').textContent = currentSlide;
+      },
+      slideChange: function () {
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide05 .currentSlide').textContent = currentSlide;
+      },
+    }
+
+  });
+
+  const swiper07 = new Swiper('.productSlide06 .swiper-container', {
+    loop: false,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.productSlide06 .nextBtn',
+      prevEl: '.productSlide06 .prevBtn',
+    },
+    on: {
+      init: function () {
+        const totalSlides = this.slides.length;
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide06 .totalSlides').textContent = totalSlides;
+        document.querySelector('.productSlide06 .currentSlide').textContent = currentSlide;
+      },
+      slideChange: function () {
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide06 .currentSlide').textContent = currentSlide;
+      },
+    }
+
+  });
+
+  const swiper08 = new Swiper('.productSlide07 .swiper-container', {
+    loop: false,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.productSlide07 .nextBtn',
+      prevEl: '.productSlide07 .prevBtn',
+    },
+    on: {
+      init: function () {
+        const totalSlides = this.slides.length;
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide07 .totalSlides').textContent = totalSlides;
+        document.querySelector('.productSlide07 .currentSlide').textContent = currentSlide;
+      },
+      slideChange: function () {
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide07 .currentSlide').textContent = currentSlide;
+      },
+    }
+
+  });
+
+  const swiper09 = new Swiper('.productSlide08 .swiper-container', {
+    loop: false,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.productSlide08 .nextBtn',
+      prevEl: '.productSlide08 .prevBtn',
+    },
+    on: {
+      init: function () {
+        const totalSlides = this.slides.length;
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide08 .totalSlides').textContent = totalSlides;
+        document.querySelector('.productSlide08 .currentSlide').textContent = currentSlide;
+      },
+      slideChange: function () {
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide08 .currentSlide').textContent = currentSlide;
+      },
+    }
+
+  });
+
+  const swiper10 = new Swiper('.productSlide09 .swiper-container', {
+    loop: false,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.productSlide09 .nextBtn',
+      prevEl: '.productSlide09 .prevBtn',
+    },
+    on: {
+      init: function () {
+        const totalSlides = this.slides.length;
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide09 .totalSlides').textContent = totalSlides;
+        document.querySelector('.productSlide09 .currentSlide').textContent = currentSlide;
+      },
+      slideChange: function () {
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide09 .currentSlide').textContent = currentSlide;
+      },
+    }
+
+  });
+
+  const swiper11 = new Swiper('.productSlide10 .swiper-container', {
+    loop: false,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.productSlide10 .nextBtn',
+      prevEl: '.productSlide10 .prevBtn',
+    },
+    on: {
+      init: function () {
+        const totalSlides = this.slides.length;
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide10 .totalSlides').textContent = totalSlides;
+        document.querySelector('.productSlide10 .currentSlide').textContent = currentSlide;
+      },
+      slideChange: function () {
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide10 .currentSlide').textContent = currentSlide;
+      },
+    }
+
+  });
+
+  const swiper12 = new Swiper('.productSlide11 .swiper-container', {
+    loop: false,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.productSlide11 .nextBtn',
+      prevEl: '.productSlide11 .prevBtn',
+    },
+    on: {
+      init: function () {
+        const totalSlides = this.slides.length;
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide11 .totalSlides').textContent = totalSlides;
+        document.querySelector('.productSlide11 .currentSlide').textContent = currentSlide;
+      },
+      slideChange: function () {
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide11 .currentSlide').textContent = currentSlide;
+      },
+    }
+
+  });
+
+  const swiper13 = new Swiper('.productSlide12 .swiper-container', {
+    loop: false,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.productSlide12 .nextBtn',
+      prevEl: '.productSlide12 .prevBtn',
+    },
+    on: {
+      init: function () {
+        const totalSlides = this.slides.length;
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide12 .totalSlides').textContent = totalSlides;
+        document.querySelector('.productSlide12 .currentSlide').textContent = currentSlide;
+      },
+      slideChange: function () {
+        const currentSlide = this.activeIndex + 1;
+        document.querySelector('.productSlide12 .currentSlide').textContent = currentSlide;
+      },
+    }
+
+  });
+
+
+  /* 탭 */
   $('.productWrap .productList01 li h3 a').click(function () {
 
     if ($(this).parent().hasClass('active')) {
@@ -91,7 +291,6 @@ $(function () {
 
   });
 
-  /* 탭 */
   $('.tabBox01 .tabContent01').hide();
   $('.tabBox01 .tabContent01').first().show();
 
